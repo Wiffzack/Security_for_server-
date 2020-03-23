d0eb0905a916cd6450273b35a9e5b7a10e5dc7d8
Wiffzack <wiffzackius@gmail.com>
2020-03-23 22:38:26 +0100

    Update create_sha256.sh

--- create_sha256.sh
+++ create_sha256.sh
@@ -1,18 +1,22 @@
 echo "First Parameter path to the files"
+
 FILES=$1
-if [ -z "$FILES" ]
-then
+if [ -z "$FILES" ]; then
         echo "No Input"
         exit 1
 fi
+
 prefix=$FILES
 cd $FILES
-for f in *.js
-do
-        if [[ $f == *.js ]];then
-                path=$(realpath $f)
-                string=${path/#$prefix}
-                sha256=$(openssl dgst -sha256 -binary $f | openssl base64 -A)
-                echo '<script src="'$string'" integrity="sha256-'$sha256'" crossorigin="anonymous"></script>'
-        fi
+for d in */ ; do
+        cd $FILES/$d
+        for f in *.*
+        do
+                if [[ $f == *.js ]];then
+                        path=$(realpath $f)
+                        string=${path/#$prefix}
+                        sha256=$(openssl dgst -sha256 -binary $f | openssl base64 -A)
+                        echo '<script src="'$string'" integrity="sha256-'$sha256'" crossorigin="anonymous"></script>'
+                fi
+        done
 done
